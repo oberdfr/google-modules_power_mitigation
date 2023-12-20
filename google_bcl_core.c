oberdfr/google-modules_power_mitigation

commit 605f326a38187f5dc2ed440de8ea3a4022cd57d9

bcl: Separate clock divider ratio lock per system
There is contention in between turning on TPU/GPU/AUR/CPU causing lock
to fail.  Separating the locks out per system.

Bug: 309560449
Change-Id: Ib1e53fec61f3de8206de19b449fe8c5c3497538d
Signed-off-by: George Lee <geolee@google.com>

google_bcl_core.c
@@ -432,7 +432,10 @@ static int google_bcl_remove_thermal(struct bcl_device *bcl_dev)
 	}
 	mutex_destroy(&bcl_dev->data_logging_lock);
 	mutex_destroy(&bcl_dev->state_trans_lock);
-	mutex_destroy(&bcl_dev->ratio_lock);
+	mutex_destroy(&bcl_dev->cpu_ratio_lock);
+	mutex_destroy(&bcl_dev->gpu_ratio_lock);
+	mutex_destroy(&bcl_dev->tpu_ratio_lock);
+	mutex_destroy(&bcl_dev->aur_ratio_lock);
 	mutex_destroy(&bcl_dev->sysreg_lock);
 
 	return 0;
@@ -482,7 +485,7 @@ struct bcl_device *google_retrieve_bcl_handle(void)
 }
 EXPORT_SYMBOL_GPL(google_retrieve_bcl_handle);
 
-static int google_init_ratio(struct bcl_device *data, enum SUBSYSTEM_SOURCE idx)
+static int google_init_ratio(struct bcl_device *data, enum SUBSYSTEM_SOURCE idx, struct mutex lock)
 {
 	void __iomem *addr;
 
@@ -495,7 +498,7 @@ static int google_init_ratio(struct bcl_device *data, enum SUBSYSTEM_SOURCE idx)
 	if (idx < SUBSYSTEM_TPU)
 		return -EIO;
 
-	mutex_lock(&data->ratio_lock);
+	mutex_lock(&lock);
 	if (idx != SUBSYSTEM_AUR) {
 		addr = data->core_conf[idx].base_mem + CLKDIVSTEP_CON_HEAVY;
 		__raw_writel(data->core_conf[idx].con_heavy, addr);
@@ -510,26 +513,26 @@ static int google_init_ratio(struct bcl_device *data, enum SUBSYSTEM_SOURCE idx)
 	__raw_writel(data->core_conf[idx].clk_out, addr);
 	data->core_conf[idx].clk_stats = __raw_readl(data->core_conf[idx].base_mem +
 						     clk_stats_offset[idx]);
-	mutex_unlock(&data->ratio_lock);
+	mutex_unlock(&lock);
 
 	return 0;
 }
 
 int google_init_tpu_ratio(struct bcl_device *data)
 {
-	return google_init_ratio(data, SUBSYSTEM_TPU);
+	return google_init_ratio(data, SUBSYSTEM_TPU, data->tpu_ratio_lock);
 }
 EXPORT_SYMBOL_GPL(google_init_tpu_ratio);
 
 int google_init_gpu_ratio(struct bcl_device *data)
 {
-	return google_init_ratio(data, SUBSYSTEM_GPU);
+	return google_init_ratio(data, SUBSYSTEM_GPU, data->gpu_ratio_lock);
 }
 EXPORT_SYMBOL_GPL(google_init_gpu_ratio);
 
 int google_init_aur_ratio(struct bcl_device *data)
 {
-	return google_init_ratio(data, SUBSYSTEM_AUR);
+	return google_init_ratio(data, SUBSYSTEM_AUR, data->aur_ratio_lock);
 }
 EXPORT_SYMBOL_GPL(google_init_aur_ratio);
 
@@ -1669,7 +1672,10 @@ static int google_bcl_init_instruction(struct bcl_device *bcl_dev)
 	}
 
 	mutex_init(&bcl_dev->state_trans_lock);
-	mutex_init(&bcl_dev->ratio_lock);
+	mutex_init(&bcl_dev->cpu_ratio_lock);
+	mutex_init(&bcl_dev->tpu_ratio_lock);
+	mutex_init(&bcl_dev->gpu_ratio_lock);
+	mutex_init(&bcl_dev->aur_ratio_lock);
 	google_bcl_enable_vdroop_irq(bcl_dev);
 
 	bcl_dev->base_add_mem[SUBSYSTEM_CPU0] = devm_ioremap(bcl_dev->device, ADD_CPUCL0, SZ_128);
@@ -1833,18 +1839,12 @@ static void google_bcl_parse_dtree(struct bcl_device *bcl_dev)
 	}
 #endif
 
-	if (bcl_disable_power(bcl_dev, SUBSYSTEM_CPU2)) {
-		if (google_bcl_init_clk_div(bcl_dev, SUBSYSTEM_CPU2,
-					    bcl_dev->core_conf[SUBSYSTEM_CPU2].clkdivstep) != 0)
-			dev_err(bcl_dev->device, "CPU2 Address is NULL\n");
-		bcl_enable_power(bcl_dev, SUBSYSTEM_CPU2);
-	}
-	if (bcl_disable_power(bcl_dev, SUBSYSTEM_CPU1)) {
-		if (google_bcl_init_clk_div(bcl_dev, SUBSYSTEM_CPU1,
-					    bcl_dev->core_conf[SUBSYSTEM_CPU1].clkdivstep) != 0)
-			dev_err(bcl_dev->device, "CPU1 Address is NULL\n");
-		bcl_enable_power(bcl_dev, SUBSYSTEM_CPU1);
-	}
+	if (google_bcl_init_clk_div(bcl_dev, SUBSYSTEM_CPU2,
+				    bcl_dev->core_conf[SUBSYSTEM_CPU2].clkdivstep) != 0)
+		dev_err(bcl_dev->device, "CPU2 Address is NULL\n");
+	if (google_bcl_init_clk_div(bcl_dev, SUBSYSTEM_CPU1,
+				    bcl_dev->core_conf[SUBSYSTEM_CPU1].clkdivstep) != 0)
+		dev_err(bcl_dev->device, "CPU1 Address is NULL\n");
 	if (google_bcl_init_clk_div(bcl_dev, SUBSYSTEM_CPU0,
 	                            bcl_dev->core_conf[SUBSYSTEM_CPU0].clkdivstep) != 0)
 		dev_err(bcl_dev->device, "CPU0 Address is NULL\n");

bcl.h
@@ -235,7 +235,10 @@ struct bcl_device {
 	struct i2c_client *intf_pmic_i2c;
 	struct i2c_client *irq_pmic_i2c;
 
-	struct mutex ratio_lock;
+	struct mutex cpu_ratio_lock;
+	struct mutex tpu_ratio_lock;
+	struct mutex gpu_ratio_lock;
+	struct mutex aur_ratio_lock;
 	struct bcl_core_conf core_conf[SUBSYSTEM_SOURCE_MAX];
 
 	bool batt_psy_initialized;

google_bcl_sysfs.c
@@ -666,13 +666,12 @@ static ssize_t enable_mitigation_store(struct device *dev, struct device_attribu
 		bcl_dev->core_conf[SUBSYSTEM_AUR].clkdivstep |= 0x1;
 		for (i = 0; i < CPU_CLUSTER_MAX; i++) {
 			addr = bcl_dev->core_conf[i].base_mem + CLKDIVSTEP;
-			mutex_lock(&bcl_dev->ratio_lock);
-			if (bcl_disable_power(bcl_dev, i)) {
-				reg = __raw_readl(addr);
-				__raw_writel(reg | 0x1, addr);
-				bcl_enable_power(bcl_dev, i);
-			}
-			mutex_unlock(&bcl_dev->ratio_lock);
+			ret = cpu_sfr_read(bcl_dev, i, addr, &reg);
+			if (ret < 0)
+				return ret;
+			ret = cpu_sfr_write(bcl_dev, i, addr, reg | 0x1);
+			if (ret < 0)
+				return ret;
 		}
 		for (i = 0; i < TRIGGERED_SOURCE_MAX; i++)
 			if (bcl_dev->zone[i] && i != BATOILO)
@@ -683,13 +682,12 @@ static ssize_t enable_mitigation_store(struct device *dev, struct device_attribu
 		bcl_dev->core_conf[SUBSYSTEM_AUR].clkdivstep &= ~(1 << 0);
 		for (i = 0; i < CPU_CLUSTER_MAX; i++) {
 			addr = bcl_dev->core_conf[i].base_mem + CLKDIVSTEP;
-			mutex_lock(&bcl_dev->ratio_lock);
-			if (bcl_disable_power(bcl_dev, i)) {
-				reg = __raw_readl(addr);
-				__raw_writel(reg & ~(1 << 0), addr);
-				bcl_enable_power(bcl_dev, i);
-			}
-			mutex_unlock(&bcl_dev->ratio_lock);
+			ret = cpu_sfr_read(bcl_dev, i, addr, &reg);
+			if (ret < 0)
+				return ret;
+			ret = cpu_sfr_write(bcl_dev, i, addr, reg | 0x1);
+			if (ret < 0)
+				return ret;
 		}
 		for (i = 0; i < TRIGGERED_SOURCE_MAX; i++)
 			if (bcl_dev->zone[i] && i != BATOILO)
@@ -1880,7 +1878,7 @@ static ssize_t clk_ratio_store(struct bcl_device *bcl_dev, enum RATIO_SOURCE idx
 	}
 	if (addr == NULL)
 		return -EIO;
-	ret = cpu_sfr_write(bcl_dev, idx, addr, value);
+	ret = cpu_sfr_write(bcl_dev, sub_idx, addr, value);
 
 	if (ret < 0)
 		return ret;

google_bcl_util.c
@@ -19,6 +19,7 @@
 #include <linux/mfd/samsung/s2mpg1415-register.h>
 #endif
 
+#include <soc/google/cal-if.h>
 #include <soc/google/exynos-cpupm.h>
 #include <soc/google/exynos-pm.h>
 #include <soc/google/exynos-pmu-if.h>
@@ -58,21 +59,27 @@ int meter_write(int pmic, struct bcl_device *bcl_dev, u8 reg, u8 value)
 
 int cpu_sfr_write(struct bcl_device *bcl_dev, int idx, void __iomem *addr, unsigned int value)
 {
-	if (!bcl_disable_power(bcl_dev, idx))
+	mutex_lock(&bcl_dev->cpu_ratio_lock);
+	if (!bcl_disable_power(bcl_dev, idx)) {
+		mutex_unlock(&bcl_dev->cpu_ratio_lock);
 		return -EIO;
-	mutex_lock(&bcl_dev->ratio_lock);
+	}
 	__raw_writel(value, addr);
-	mutex_unlock(&bcl_dev->ratio_lock);
 	bcl_enable_power(bcl_dev, idx);
+	mutex_unlock(&bcl_dev->cpu_ratio_lock);
 	return 0;
 }
 
 int cpu_sfr_read(struct bcl_device *bcl_dev, int idx, void __iomem *addr, unsigned int *reg)
 {
-	if (!bcl_disable_power(bcl_dev, idx))
+	mutex_lock(&bcl_dev->cpu_ratio_lock);
+	if (!bcl_disable_power(bcl_dev, idx)) {
+		mutex_unlock(&bcl_dev->cpu_ratio_lock);
 		return -EIO;
+	}
 	*reg = __raw_readl(addr);
 	bcl_enable_power(bcl_dev, idx);
+	mutex_unlock(&bcl_dev->cpu_ratio_lock);
 
 	return 0;
 }
@@ -155,19 +162,16 @@ bool bcl_disable_power(struct bcl_device *bcl_dev, int cluster)
 	if (IS_ENABLED(CONFIG_SOC_ZUMA) || IS_ENABLED(CONFIG_SOC_GS201)) {
 		int i;
 		if (cluster == SUBSYSTEM_CPU1) {
+			if (cal_cpu_enable(bcl_dev->cpu1_cluster) < 0)
+				return false;
 			for (i = bcl_dev->cpu1_cluster; i < bcl_dev->cpu2_cluster; i++) {
-				if (bcl_is_cluster_on(bcl_dev, i) == true) {
-					disable_power_mode(i, POWERMODE_TYPE_CLUSTER);
-				} else
-					return false;
+				disable_power_mode(i, POWERMODE_TYPE_CLUSTER);
 			}
 		}
 		else if (cluster == SUBSYSTEM_CPU2) {
-			if (bcl_is_cluster_on(bcl_dev, bcl_dev->cpu2_cluster) == true) {
-				disable_power_mode(bcl_dev->cpu2_cluster, POWERMODE_TYPE_CLUSTER);
-			} else {
+			if (cal_cpu_enable(bcl_dev->cpu2_cluster) < 0)
 				return false;
-			}
+			disable_power_mode(bcl_dev->cpu2_cluster, POWERMODE_TYPE_CLUSTER);
 		}
 	}
 	return true;
@@ -178,18 +182,16 @@ bool bcl_enable_power(struct bcl_device *bcl_dev, int cluster)
 	if (IS_ENABLED(CONFIG_SOC_ZUMA) || IS_ENABLED(CONFIG_SOC_GS201)) {
 		int i;
 		if (cluster == SUBSYSTEM_CPU1) {
+			if (cal_cpu_enable(bcl_dev->cpu1_cluster) < 0)
+				return false;
 			for (i = bcl_dev->cpu1_cluster; i < bcl_dev->cpu2_cluster; i++) {
-				if (bcl_is_cluster_on(bcl_dev, i) == true) {
-					enable_power_mode(i, POWERMODE_TYPE_CLUSTER);
-				} else
-					return false;
+				enable_power_mode(i, POWERMODE_TYPE_CLUSTER);
 			}
 		}
 		else if (cluster == SUBSYSTEM_CPU2) {
-			if (bcl_is_cluster_on(bcl_dev, bcl_dev->cpu2_cluster) == true) {
-				enable_power_mode(bcl_dev->cpu2_cluster, POWERMODE_TYPE_CLUSTER);
-			} else
+			if (cal_cpu_enable(bcl_dev->cpu2_cluster) < 0)
 				return false;
+			enable_power_mode(bcl_dev->cpu2_cluster, POWERMODE_TYPE_CLUSTER);
 		}
 	}
 	return true;

commit 49e2ff0d8d99cc8cccbde9f9c160fabf417d6717

bcl: Pass mutexes by reference
A mutex can only work as a mutex if all mutex_lock() and all
mutex_unlock() calls modify the same instance. Hence pass struct mutex
by reference instead of by value.

Bug: 316367811
Fixes: 605f326a3818 ("bcl: Separate clock divider ratio lock per system")
Change-Id: I8cdc60040e694f791314de417274ebd2779cb858
Signed-off-by: Bart Van Assche <bvanassche@acm.org>

google_bcl_core.c
@@ -484,7 +484,8 @@ struct bcl_device *google_retrieve_bcl_handle(void)
 }
 EXPORT_SYMBOL_GPL(google_retrieve_bcl_handle);
 
-static int google_init_ratio(struct bcl_device *data, enum SUBSYSTEM_SOURCE idx, struct mutex lock)
+static int google_init_ratio(struct bcl_device *data, enum SUBSYSTEM_SOURCE idx,
+			     struct mutex *lock)
 {
 	void __iomem *addr;
 
@@ -500,7 +501,7 @@ static int google_init_ratio(struct bcl_device *data, enum SUBSYSTEM_SOURCE idx,
 	if (idx < SUBSYSTEM_TPU)
 		return -EIO;
 
-	mutex_lock(&lock);
+	mutex_lock(lock);
 	if (idx != SUBSYSTEM_AUR) {
 		addr = data->core_conf[idx].base_mem + CLKDIVSTEP_CON_HEAVY;
 		__raw_writel(data->core_conf[idx].con_heavy, addr);
@@ -515,26 +516,26 @@ static int google_init_ratio(struct bcl_device *data, enum SUBSYSTEM_SOURCE idx,
 	__raw_writel(data->core_conf[idx].clk_out, addr);
 	data->core_conf[idx].clk_stats = __raw_readl(data->core_conf[idx].base_mem +
 						     clk_stats_offset[idx]);
-	mutex_unlock(&lock);
+	mutex_unlock(lock);
 
 	return 0;
 }
 
 int google_init_tpu_ratio(struct bcl_device *data)
 {
-	return google_init_ratio(data, SUBSYSTEM_TPU, data->tpu_ratio_lock);
+	return google_init_ratio(data, SUBSYSTEM_TPU, &data->tpu_ratio_lock);
 }
 EXPORT_SYMBOL_GPL(google_init_tpu_ratio);
 
 int google_init_gpu_ratio(struct bcl_device *data)
 {
-	return google_init_ratio(data, SUBSYSTEM_GPU, data->gpu_ratio_lock);
+	return google_init_ratio(data, SUBSYSTEM_GPU, &data->gpu_ratio_lock);
 }
 EXPORT_SYMBOL_GPL(google_init_gpu_ratio);
 
 int google_init_aur_ratio(struct bcl_device *data)
 {
-	return google_init_ratio(data, SUBSYSTEM_AUR, data->aur_ratio_lock);
+	return google_init_ratio(data, SUBSYSTEM_AUR, &data->aur_ratio_lock);
 }
 EXPORT_SYMBOL_GPL(google_init_aur_ratio);
 