2021ef4c2f2e9c542a9e2c7333f999268d03de2b
George Lee <geolee@google.com>
2024-04-14 02:58:46 +0000

    bcl: Adjust BATOILO level to 4.8A when plugged in
    Due to tolerance, when device is plugged into USB, BATOILO level needs
    to be reduced by 200mA.
    
    Bug: 334171387
    Change-Id: I6f3c1da52bd410a8c971795d2fb0e725dfde4b65
    Signed-off-by: George Lee <geolee@google.com>

--- google_bcl_votable.c
+++ google_bcl_votable.c
@@ -13,10 +13,16 @@
 #include "bcl.h"
 
 #define BCL_WLC "BCL_WLC"
+#define BCL_USB "BCL_USB"
 
 enum {
 	WLC_ENABLED_TX,
 	WLC_DISABLED_TX,
+};
+
+enum {
+	USB_PLUGGED,
+	USB_UNPLUGGED,
 };
 
 static int google_bcl_wlc_votable_callback(struct gvotable_election *el,
@@ -27,13 +33,37 @@ static int google_bcl_wlc_votable_callback(struct gvotable_election *el,
 	u8 wlc_tx_enable = (long)value ? WLC_ENABLED_TX : WLC_DISABLED_TX;
 
 
-	ret = max77779_adjust_batoilo_lvl(bcl_dev, wlc_tx_enable);
-	if (ret < 0) {
-		dev_err(bcl_dev->device, "BATOILO cannot be adjusted\n");
-		return ret;
+	if (bcl_dev->ifpmic == MAX77779) {
+		ret = max77779_adjust_batoilo_lvl(bcl_dev, wlc_tx_enable,
+	                                  	  bcl_dev->batt_irq_conf1.batoilo_wlc_trig_lvl,
+	                                  	  bcl_dev->batt_irq_conf2.batoilo_wlc_trig_lvl);
+		if (ret < 0) {
+			dev_err(bcl_dev->device, "BATOILO cannot be adjusted\n");
+			return ret;
+		}
 	}
+	/* b/335695535 outlines max77759 configuration */
 
 	return 0;
+}
+
+static int google_bcl_usb_votable_callback(struct gvotable_election *el,
+					   const char *reason, void *value)
+{
+	int ret = 0;
+	struct bcl_device *bcl_dev = gvotable_get_data(el);
+	u8 usb_enable = (long)value ? USB_PLUGGED: USB_UNPLUGGED;
+
+	if (bcl_dev->ifpmic == MAX77779) {
+		ret = max77779_adjust_batoilo_lvl(bcl_dev, usb_enable,
+	                                  	  bcl_dev->batt_irq_conf1.batoilo_usb_trig_lvl,
+	                                  	  bcl_dev->batt_irq_conf2.batoilo_usb_trig_lvl);
+		if (ret < 0)
+			dev_err(bcl_dev->device, "USB: BATOILO cannot be adjusted\n");
+	}
+	/* b/335695535 outlines max77759 configuration */
+
+	return ret;
 }
 
 int google_bcl_setup_votable(struct bcl_device *bcl_dev)
@@ -49,10 +79,21 @@ int google_bcl_setup_votable(struct bcl_device *bcl_dev)
 	}
 	gvotable_set_vote2str(bcl_dev->toggle_wlc, gvotable_v2s_int);
 	gvotable_election_set_name(bcl_dev->toggle_wlc, BCL_WLC);
+
+	bcl_dev->toggle_usb = gvotable_create_bool_election(NULL, google_bcl_usb_votable_callback,
+							    bcl_dev);
+	if (IS_ERR_OR_NULL(bcl_dev->toggle_usb)) {
+		ret = PTR_ERR(bcl_dev->toggle_usb);
+		dev_err(bcl_dev->device, "no toggle_usb votable (%d)\n", ret);
+		return ret;
+	}
+	gvotable_set_vote2str(bcl_dev->toggle_usb, gvotable_v2s_int);
+	gvotable_election_set_name(bcl_dev->toggle_usb, BCL_USB);
 	return 0;
 }
 
 void google_bcl_remove_votable(struct bcl_device *bcl_dev)
 {
 	gvotable_destroy_election(bcl_dev->toggle_wlc);
+	gvotable_destroy_election(bcl_dev->toggle_usb);
 }
